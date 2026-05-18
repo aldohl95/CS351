87d026c666fac321ec779d78ee801730c312b6b9
Tevita Mathias <mathias.tevita@icloud.com>
2026-05-17 18:24:53 -0700

    added status model class

---
+++ backend/zip/src/main/java/com/servicerequest/model/Priority.java
@@ -0,0 +1,7 @@
+package com.servicerequest.model;
+
+public enum Priority {
+    HIGH,
+    MEDIUM,
+    LOW
+}
---
+++ backend/zip/src/main/java/com/servicerequest/model/Status.java
@@ -0,0 +1,7 @@
+package com.servicerequest.model;
+
+public enum Status {
+    OPEN,
+    CLOSED,
+    IN_PROGRESS
+}
