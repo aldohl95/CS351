77ab8014d99035bb7784e9620eef39b40b74308c
Tevita Mathias <mathias.tevita@icloud.com>
2026-05-20 14:26:49 -0700

    updaterequest dto class created

---
+++ backend/zip/src/main/java/com/servicerequest/service/UpdateRequestDTO.java
@@ -0,0 +1,36 @@
+package com.servicerequest.service;
+
+public class UpdateRequestDTO {
+
+    private String status;
+    private String priority;
+    private String changedBy;
+
+    public UpdateRequestDTO(){
+
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getChangedBy() {
+        return changedBy;
+    }
+
+    public void setChangedBy(String changedBy) {
+        this.changedBy = changedBy;
+    }
+}
